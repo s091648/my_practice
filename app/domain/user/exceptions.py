a18bb8795a4422a77f553c591133618da1ae8ee6
Weifan LIU <s091648@gmail.com>
2025-04-10 06:33:11 +0900

    align validation error code with Pydantic BaseModel

--- app/domain/user/exceptions.py
+++ app/domain/user/exceptions.py
@@ -2,11 +2,11 @@ from app.core.exceptions import AppBaseException
 
 
 class EmptyUserNameError(AppBaseException):
-    status_code: int = 400
+    status_code: int = 422
     detail: str = "User name cannot be empty."
     exception_type: str = "EmptyUserNameError"
 
 class NegativeUserAgeError(AppBaseException):
-    status_code: int = 400
+    status_code: int = 422
     detail: str = "User age cannot be negative."
     exception_type: str = "NegativeUserAgeError"
