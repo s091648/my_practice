48acca8656ba840c9479f0c3f371f061b0faf4e1
Weifan LIU <s091648@gmail.com>
2025-04-26 16:27:00 +0900

    add comment

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -3,5 +3,6 @@ class AppBaseException(Exception):
     detail: str = "Internal server error"
     exception_type: str = "AppBaseException"
 
+    # TODO: 直接回傳JSON(status_code, detail)
     def to_response(self) -> dict:
         return {"detail": f"{self.exception_type}: {self.detail}"}
